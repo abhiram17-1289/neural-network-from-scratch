19489dcfe9c9112e083f05ba74eb007c7fdfa0a5
Abhirama Karthikeya <abhiramakarthikeya@gmail.com>
2024-08-04 01:15:57 +0530

    Commit after building all the necessary functions. Implementation to be done now

--- nn_from_scratch.py
+++ nn_from_scratch.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
 #Here I am going to implement a deep neural network from scratch
 #This is based on my understandings and learning from the Deep Learning Specialization by Deeplearning.AI
@@ -108,5 +109,111 @@ def compute_cost(AL, Y):
     cost = np.squeeze(cost)
 
     return cost
+
+#Start of backward propagation
+def backward_linear(dZ, linear_cache):
+    """
+    Input -- dZ, linear_cache
+    output -- dW, db, dA_prev
+
+    """
+    A_prev, W, b = linear_cache
+    m = A_prev.shape[1] #Shape of any A is (nl, m)
+    dW = (1 / m) * np.dot(dZ, A_prev.T) ##Assuming that the cost derivative dZ or dA are not scaled by 1/m
+    db = (1 / m) * np.sum(dZ, axis = 1, keepdims=True) #Sum of dZ over all examples, since for one example db = dZ
+    dA_prev = np.dot(W.T, dZ)
+
+    return dA_prev, dW, db
+
+#Calculate the derivatives of common functions
+#dZ = dA * g'(Z)
+
+#Sigmoid
+def sigmoid_backwards(dA, activation_cache):
+    der_sigmoid = sigmoid(activation_cache) * (1 - sigmoid(activation_cache)) #Derivative of a sigmoid
+    return np.multiply(dA, der_sigmoid)
+
+#Tanh
+def tanh_backwards(dA, activation_cache):
+    der_tanh = 1 - (tanh(activation_cache) ** 2)
+    return np.multiply(dA, der_tanh)
+
+#ReLU
+def relu_backwards(dA, activation_cache):
+    der_relu = np.where(activation_cache > 0, 1, 0)
+    return np.multiply(dA, der_relu)
+
+#Incorporate the derivative of the activation function 
+def backward_activation(dA, cache, activation):
+    """
+    Input -- dA, cache (linear + activation of that layer), activation type of that layer
+    output -- dA_prev, dW, db
+
+    """
+
+    linear_cache, activation_cache = cache
+
+    if activation == "relu":
+        dZ = relu_backwards(dA, activation_cache)
+        dA_prev, dW, db = backward_linear(dZ, linear_cache)
+    
+    elif activation == "sigmoid":
+        dZ = sigmoid_backwards(dA, activation_cache)
+        dA_prev, dW, db = backward_linear(dZ, linear_cache)
+    
+    elif activation == "tanh":
+        dZ = tanh_backwards(dA, activation_cache)
+        dA_prev, dW, db = backward_linear(dZ, linear_cache)
+
+    return dA_prev, dW, db
+
+def backward_propagation(AL, Y, caches):
+    """
+    Input -- AL, Y, caches of all layers
+    output -- gradients dictionary containing dA_prev, dW, db
+
+    """
+
+    grads = {}
+    L = len(caches) #Number of caches is number of layers other than input
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)
+
+    #Calculate dAL for first layer from last based on cost function (Ignoring 1/m here, added later)
+    dAL = - np.divide(Y, AL) + np.divide((1-Y), (1-AL))
+
+    current_cache = caches[L-1] #Last/Current cache
+    dA_prev_temp, dW_temp, db_temp = backward_activation(dAL, current_cache, "sigmoid")
+    grads["dA" + str(L-1)] = dA_prev_temp #Activation of previous, i.e L-1 layer. dAL is already known
+    grads["dW" + str(L)] = dW_temp #Weights of current Lth layer
+    grads["db" + str(L)] = db_temp #Biases of current Lth layer
+
+    #Go through each layer from last but one to first
+    for l in range(L-2, -1, -1): #l goes from L-2 to 0
+        current_cache = caches[l] 
+        dA_prev_temp, dW_temp, db_temp = backward_activation(grads["dA" + str(l+1)], current_cache, "relu") #Takes activation of l+1 layer as input. Eg, L-2 takes L-1 layer as input
+        grads["dA" + str(l)] = dA_prev_temp #Activation of previous, i.e 
+        grads["dW" + str(l+1)] = dW_temp #Weights of current Lth layer
+        grads["db" + str(l+1)] = db_temp #Biases of current Lth layer
+
+    return grads
+
+#Finally, we update the parameters
+
+def update_parameters(params, grads, learning_rate):
+
+    parameters = copy.deepcopy(parameters)
+    L = len(parameters) // 2
+
+    for l in range(L): #Goes from 0 to L-1, do l+ 1
+        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate * (grads["dW" + str(l+1)])
+        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * (grads["db" + str(l+1)])
+
+    return parameters
+
+
+
+
+
 
 
