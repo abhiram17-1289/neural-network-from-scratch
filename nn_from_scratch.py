9e8ce9fec41b4c5820420a8010a65b230e34f415
Abhirama Karthikeya <abhiramakarthikeya@gmail.com>
2024-08-03 18:36:39 +0530

    Initial Commit, just starting out

---
+++ nn_from_scratch.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Here I am going to implement a deep neural network from scratch
+#This is based on my understandings and learning from the Deep Learning Specialization by Deeplearning.AI
+
+
